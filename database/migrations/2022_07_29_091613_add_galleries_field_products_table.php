21529615a932990b226e8df1c84acb0971ebf128
thanhle <thanhle09t2bkdn@gmail.com>
2022-08-15 18:18:02 +0700

    nullable

--- database/migrations/2022_07_29_091613_add_galleries_field_products_table.php
+++ database/migrations/2022_07_29_091613_add_galleries_field_products_table.php
@@ -14,7 +14,7 @@ class AddGalleriesFieldProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->json('galleries');
+            $table->json('galleries')->nullable();
         });
     }
 
